ee215d7d1f67dbe5edb4a0f756eec020a822c839
Jake <jakelever@gmail.com>
2021-03-26 13:51:13 +0000

    - ADDED OUTPUTS

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ with open('query_list.csv', newline='') as csvfile:
     for row in reader:
         if len(row) == 4:
             index, query, start_date, end_date = row[0].strip(), row[1].strip(), row[2].strip(), row[3].strip()
-            tweets = twitter.get_tweets(start_date, end_date, query)
-            if tweets is not None:
-                for tweet in tweets:
-                    print(tweet)
+            with open('output/{}_tweets.csv'.format(index), 'w', newline='') as csv_out:
+                writer = csv.writer(csv_out, delimiter=',', quoting= csv.QUOTE_MINIMAL)
+                writer.writerow(['Tweet_ID', 'Date', 'Text', 'Author_ID'])
+                tweets = twitter.get_tweets(start_date, end_date, query)
+                if tweets is not None:
+                    for tweet in tweets:
+                        # print(tweet)
+                        text = tweet.full_text.replace('"', "'").replace('\n', ' ')
+                        writer.writerow([tweet.tweet_id, tweet.date,  text, tweet.author_id])
+
         else:
             print('Error reading CSV row {}: Contains incorrect number of columns ({})'.format(index, len(row)))
     print('Queries searched successfully. Tweets stored in output folder.')
